49084a6956168cf861c4942fab434af673821a70
민경원 <sojoonghan2@naver.com>
2025-12-15 15:42:48 +0900

    feat: add ItemUseEffect

---
+++ StrangeShowdown/Source/StrangeShowdown/Item/STItemUseEffect.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/STItemUseEffect.h"
+#include "STItemUseEffect.h"
+
+void USTItemUseEffect::Use_Implementation(AActor* User)
+{
+
+}
---
+++ StrangeShowdown/Source/StrangeShowdown/Item/STItemUseEffect.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "STItemUseEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class STRANGESHOWDOWN_API USTItemUseEffect : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "Item")
+	void Use(AActor* User);
+
+	virtual void Use_Implementation(AActor* User);
+};
