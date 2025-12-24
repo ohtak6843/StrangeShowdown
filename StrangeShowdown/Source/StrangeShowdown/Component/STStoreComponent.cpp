4df5b5fb858808c63e3065afdbf89a39395d00b3
민경원 <sojoonghan2@naver.com>
2025-12-24 14:23:52 +0900

    feat: add Store UI

--- StrangeShowdown/Source/StrangeShowdown/Component/STStoreComponent.cpp
+++ StrangeShowdown/Source/StrangeShowdown/Component/STStoreComponent.cpp
@@ -1,70 +1,66 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Component/STStoreComponent.h"
+#include "Character/STLocalPlayer.h"
 #include "Component/STStatComponent.h"
 #include "Component/STInventoryComponent.h"
+#include "Item/STItemDataAssetBase.h"
 
-// Sets default values for this component's properties
 USTStoreComponent::USTStoreComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
 void USTStoreComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	OwnerPlayer = Cast<ASTLocalPlayer>(GetOwner());
 
-	// CurrentItem 초기화
-	CurrentItems.SetNum(5);
-
-	InitializeStore();
+	Slots.SetNum(SlotCount);
+	StoreItemPool.SetNum(SlotCount);
 }
 
-void USTStoreComponent::InitializeStore()
+void USTStoreComponent::InitStore()
 {
-	// 여기서 아이템 데이터 에셋을 서버로부터 받아와서
-	// 블루프린트에서 이미지와 가격을 설정해야 함
-
-	// 임시로 모든 아이템에 0번 아이템 할당
 	if (StoreItemPool.Num() == 0)
-		return;
-
-	for (int32 i = 0; i < CurrentItems.Num(); i++)
 	{
-		CurrentItems[i].ItemData = StoreItemPool[0];
-		CurrentItems[i].Price = CurrentItems[i].ItemData->GoldCost;
-		CurrentItems[i].bSold = false;
+		UE_LOG(LogTemp, Warning, TEXT("StoreItemPool is empty"));
+		return;
+	}
+
+	for (int32 i = 0; i < Slots.Num(); i++)
+	{
+		Slots[i].ItemData = StoreItemPool[0];
+		Slots[i].bSold = false;
 	}
 }
 
 bool USTStoreComponent::BuyItem(int32 Index)
 {
-	// 플레이어 골드 차감
-	OwnerPlayer->StatComp->AddGold(-CurrentItems[Index].Price);
+	if (!Slots.IsValidIndex(Index))
+		return false;
 
-	// 아이템 구매 처리
-	CurrentItems[Index].bSold = true;
+	if (Slots[Index].bSold)
+		return false;
 
-	// 아이템 인벤토리로 이동
-	OwnerPlayer->InventoryComp->AddItem(CurrentItems[Index].ItemData, 1);
+	if (!OwnerPlayer || !OwnerPlayer->StatComp || !OwnerPlayer->InventoryComp)
+		return false;
 
-	return false;
+	USTItemDataAssetBase* ItemData = Slots[Index].ItemData;
+	if (!ItemData)
+		return false;
+
+	OwnerPlayer->StatComp->AddGold(-ItemData->GoldCost);
+	OwnerPlayer->InventoryComp->AddItem(ItemData, 1);
+
+	Slots[Index].bSold = true;
+	return true;
 }
 
 void USTStoreComponent::Reroll()
 {
-	// 플레이어 골드 차감
-	OwnerPlayer->StatComp->AddGold(-RerollCost);
+	if (!OwnerPlayer || !OwnerPlayer->StatComp)
+		return;
 
-	// 상점 초기화
-	InitializeStore();
+	OwnerPlayer->StatComp->AddGold(-RerollCost);
+	InitStore();
 }
--- StrangeShowdown/Source/StrangeShowdown/Component/STStoreComponent.h
+++ StrangeShowdown/Source/StrangeShowdown/Component/STStoreComponent.h
@@ -1,63 +1,59 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Item/STItemDataAssetBase.h"
-#include "Character/STLocalPlayer.h"
 #include "STStoreComponent.generated.h"
 
+class ASTLocalPlayer;
+class USTItemDataAssetBase;
+
 USTRUCT(BlueprintType)
-struct FStoreItem
+struct FStoreSlot
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	USTItemDataAssetBase* ItemData;
+public:
+	UPROPERTY(BlueprintReadOnly)
+	USTItemDataAssetBase* ItemData = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 Price;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly)
 	bool bSold = false;
 };
 
-
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class STRANGESHOWDOWN_API USTStoreComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	USTStoreComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TArray<FStoreItem> CurrentItems;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Store")
+	TArray<USTItemDataAssetBase*> StoreItemPool;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Store")
+	TArray<FStoreSlot> Slots;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Store")
+	int32 SlotCount = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Store")
 	int32 RerollCost = 5;
 
-	UFUNCTION(BlueprintCallable)
-	void InitializeStore();
+public:
+	UFUNCTION(BlueprintCallable, Category = "Store")
+	void InitStore();
 
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Store")
 	bool BuyItem(int32 Index);
 
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Store")
 	void Reroll();
 
-	// Owner Player
-	UPROPERTY()
-	ASTLocalPlayer* OwnerPlayer;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Store")
-	TArray<USTItemDataAssetBase*> StoreItemPool;
+private:
+	ASTLocalPlayer* OwnerPlayer = nullptr;
 };
