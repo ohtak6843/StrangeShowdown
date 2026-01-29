3d7e92bf5b0874b63d73998a9fe6839a0dbd5402
민경원 <sojoonghan2@naver.com>
2026-01-29 18:48:23 +0900

    add: CustomComboBox

---
+++ StrangeShowdown/Source/StrangeShowdown/Widget/STCustomComboBox.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/STCustomComboBox.h"
+#include "STCustomComboBox.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/Border.h"
+#include "Components/ScrollBox.h"
+#include "Widget/STCustomComboBoxItem.h"
+
+void USTCustomComboBox::ToggleComboBox()
+{
+	if (bIsOpen)
+	{
+		CloseComboBox();
+	}
+	else
+	{
+		OpenComboBox();
+	}
+}
+
+void USTCustomComboBox::OpenComboBox()
+{
+	if (DropDownPanel == nullptr)
+		return;
+
+	bIsOpen = true;
+	DropDownPanel->SetVisibility(ESlateVisibility::Visible);
+}
+
+void USTCustomComboBox::CloseComboBox()
+{
+	if (DropDownPanel == nullptr)
+		return;
+
+	bIsOpen = false;
+	DropDownPanel->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void USTCustomComboBox::SelectIndex(int32 Index)
+{
+	if (!Options.IsValidIndex(Index))
+		return;
+
+	SelectedIndex = Index;
+
+	if (SelectedText)
+	{
+		SelectedText->SetVisibility(ESlateVisibility::Visible);
+		SelectedText->SetText(Options[Index].DisplayText);
+	}
+
+	CloseComboBox();
+	OnComboBoxSelected();
+	OnSelectionChanged.Broadcast(SelectedIndex);
+}
+
+void USTCustomComboBox::RefreshOptions()
+{
+	if (!ScrollBox || !ComboBoxItemClass)
+		return;
+
+	ScrollBox->ClearChildren();
+
+	for (int32 i = 0; i < Options.Num(); ++i)
+	{
+		USTCustomComboBoxItem* Item =
+			CreateWidget<USTCustomComboBoxItem>(this, ComboBoxItemClass);
+
+		if (!Item)
+			continue;
+
+		Item->SetItemData(i, Options[i].DisplayText, this);
+		ScrollBox->AddChild(Item);
+	}
+}
