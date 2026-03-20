0946dea1e97ad6da062185ca9667be30cf2cb7a4
민경원 <sojoonghan2@naver.com>
2026-03-20 16:40:46 +0900

    feat: add STMeatEffect

---
+++ StrangeShowdown/Source/StrangeShowdown/Item/STMeatEffect.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/STMeatEffect.h"
+#include "STMeatEffect.h"
+#include "Character/Player/STLocalPlayer.h"
+#include "Component/STStatComponent.h"
+
+USTMeatEffect::USTMeatEffect()
+	: Super()
+{
+	static ConstructorHelpers::FObjectFinder<USoundBase> SoundAsset(
+		TEXT("/Script/Engine.SoundWave'/Game/StrangeShowdown/Sound/Effect/SW_EatMeat.SW_EatMeat'")
+	);
+	if (SoundAsset.Succeeded())
+	{
+		UseSound = SoundAsset.Object;
+	}
+}
+
+bool USTMeatEffect::Use(ASTLocalPlayer* User, USTItemDataAssetBase* ItemData)
+{
+	if (User->StatComp->CurrentHp >= User->StatComp->MaxHp)
+	{
+		return false;
+	}
+
+	Super::Use(User, ItemData);
+
+	User->StatComp->AddHp(1);
+
+	return true;
+}
