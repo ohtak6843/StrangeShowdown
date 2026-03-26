10965034c8b90063c61f4d3cbb711e2d2189b1b2
민경원 <sojoonghan2@naver.com>
2026-03-26 19:05:48 +0900

    feat: add LobbyHUD, LobbyStatus

---
+++ StrangeShowdown/Source/StrangeShowdown/Widget/STLobbyHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STLobbyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRANGESHOWDOWN_API USTLobbyHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ StrangeShowdown/Source/StrangeShowdown/Widget/STLobbyStatusWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STLobbyStatusWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRANGESHOWDOWN_API USTLobbyStatusWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
