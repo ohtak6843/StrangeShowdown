0c56f2a38476f213fdb50999998639211f004f3e
민경원 <sojoonghan2@naver.com>
2026-03-26 18:24:10 +0900

    feat: add Lobby Level

---
+++ StrangeShowdown/Source/StrangeShowdown/Controller/STLobbyController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Controller/STLobbyController.h"
+
+void ASTLobbyController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASTLobbyController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
