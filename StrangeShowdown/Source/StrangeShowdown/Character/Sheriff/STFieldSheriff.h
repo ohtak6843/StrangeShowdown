2f0ef4f19531f72307f7848c31623678faacbbb8
민경원 <sojoonghan2@naver.com>
2026-05-21 20:24:03 +0900

    feat: add Interact Widget in FieldSheriff

--- StrangeShowdown/Source/StrangeShowdown/Character/Sheriff/STFieldSheriff.h
+++ StrangeShowdown/Source/StrangeShowdown/Character/Sheriff/STFieldSheriff.h
@@ -4,13 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Character/Sheriff/STSheriffBase.h"
+#include "Actor/STInteractableActor.h"
+#include "Components/WidgetComponent.h"
+#include "Components/SphereComponent.h"
 #include "STFieldSheriff.generated.h"
 
 /**
  * 
  */
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGhostEnter);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGhostExit);
+
 UCLASS()
-class STRANGESHOWDOWN_API ASTFieldSheriff : public ASTSheriffBase
+class STRANGESHOWDOWN_API ASTFieldSheriff : public ASTSheriffBase, public IInteractable
 {
 	GENERATED_BODY()
 
@@ -18,6 +25,42 @@ public:
 	ASTFieldSheriff();
 
 protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	virtual void Interact_Implementation(APawn* Interactor) override;
+
+	// Interact UI 위젯
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "UI", Meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* InteractWidgetComponent;
+
+	// 콜리전
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Interact")
+	USphereComponent* InteractCollision;
+
+	// Overlap 이벤트
+	UPROPERTY(BlueprintAssignable)
+	FOnGhostEnter OnGhostEnter;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGhostExit OnGhostExit;
+
+protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	uint8 bIsPossessed : 1;
+
+private:
+	UFUNCTION()
+	void HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void HandleEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY()
+	APlayerCameraManager* CachedCameraManager;
 };
